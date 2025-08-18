3c59a956e9a75eeed9fc6bc203a07f65d3c9accb
Martin <martinjuneie@gmail.com>
2025-08-18 18:04:48 +0530

    test

--- .github/scripts/concattest.py
+++ .github/scripts/concattest.py
@@ -5,6 +5,6 @@ val = 88
 output_path = os.getenv("GITHUB_OUTPUT")
 if output_path:
     with open(output_path, "a") as f:
-        f.write(f"value={val}")
+        f.write(f"valuez={val}")
 else:
     print("GITHUB_OUTPUT environment variable not set.")
